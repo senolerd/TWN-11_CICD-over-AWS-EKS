d7558841dd9cdb2029cc9567509fe667c1c85247
Senol Erdogan <senol@mac.local>
2026-08-03 03:46:21 -0500

    bug fix

--- utils.groovy
+++ utils.groovy
@@ -89,13 +89,11 @@ void deployToEKS() {
     sh "sed -i '/appVersion/c\\appVersion: $APP_VER-$BUILD_NUMBER' helm-chart/Chart.yaml"
 
     withCredentials([usernamePassword(credentialsId: env.AWS_JENKINS_ACC_KEY_ID, passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
-        echo "Getting kubeconfig file"
-        sh 'aws eks update-kubeconfig --name ${EKS_CLUSTER_NAME} --region ${AWS_REGION} --kubeconfig ${EKS_CLUSTER_NAME}.config'
+        sh "aws eks update-kubeconfig --name ${EKS_CLUSTER_NAME} --region ${AWS_REGION} --kubeconfig ${EKS_CLUSTER_NAME}.config"
     }
 
     withCredentials([usernamePassword(credentialsId: env.AWS_JENKINS_ACC_KEY_ID, passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
-        echo "Getting kubeconfig file"
-        sh 'helm list --kubeconfig ${EKS_CLUSTER_NAME}.config'
+        sh "helm list --kubeconfig ${EKS_CLUSTER_NAME}.config"
     }
 
 
