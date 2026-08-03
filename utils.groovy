81e1885721c78f1388f0425c3d8ddb4d129306ae
Senol Erdogan <senol@mac.local>
2026-08-03 11:32:47 -0500

    deploytoEks after adding Jenkins user to IAM access entries

--- utils.groovy
+++ utils.groovy
@@ -90,12 +90,8 @@ void deployToEKS() {
 
     withCredentials([usernamePassword(credentialsId: env.AWS_JENKINS_ACC_KEY_ID, passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
         sh "aws eks update-kubeconfig --name ${EKS_CLUSTER_NAME} --region ${AWS_REGION} --kubeconfig ${EKS_CLUSTER_NAME}.config"
+        helm list --kubeconfig ${EKS_CLUSTER_NAME}.config
     }
-
-    withCredentials([usernamePassword(credentialsId: env.AWS_JENKINS_ACC_KEY_ID, passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
-        sh "helm list --kubeconfig ${EKS_CLUSTER_NAME}.config"
-    }
-
 
 
     // withCredentials([file(credentialsId: env.KUBECONFIG_SECRET_FILE_ID, variable: 'KUBECONFIG')]) {
